2886ee34f014d9b40cfa1a4d788ee4f223c5e811
devil2999 <princofdestroys@gmail.com>
2021-03-05 18:57:57 +0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Fucking trash world'));
+app.get('/', (req, res) => res.send('finally'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
